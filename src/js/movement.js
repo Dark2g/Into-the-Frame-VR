4dea1ffee6a7b076d2cd29e935888070901858e0
VegetinES <vegetines.yt@gmail.com>
2026-03-23 19:05:00 +0100

    Add VR mode UI and thumbstick support for player movement

--- src/js/movement.js
+++ src/js/movement.js
@@ -1,8 +1,21 @@
   AFRAME.registerComponent('player-move', {
       init() {
         this.keys = {};
+        this.thumbstick = { x: 0, y: 0 };
         window.addEventListener('keydown', e => this.keys[e.code] = true);
         window.addEventListener('keyup', e => this.keys[e.code] = false);
+
+        // VR thumbstick support
+        this.onThumbstick = (evt) => {
+          this.thumbstick.x = evt.detail.x;
+          this.thumbstick.y = evt.detail.y;
+        };
+        this.el.sceneEl.addEventListener('loaded', () => {
+          document.querySelectorAll('[tracked-controls], [laser-controls]').forEach(ctrl => {
+            ctrl.addEventListener('thumbstickmoved', this.onThumbstick);
+            ctrl.addEventListener('trackpadmoved', this.onThumbstick);
+          });
+        });
       },
         tick() {
             const body = this.el.body;
@@ -25,6 +38,14 @@
             if (this.keys.KeyW) { vx -= dir.x; vz -= dir.z; }
             if (this.keys.KeyA) { vx += right.x; vz += right.z; }
             if (this.keys.KeyD) { vx -= right.x; vz -= right.z; }
+
+            // VR thumbstick movement
+            var tx = this.thumbstick.x;
+            var ty = this.thumbstick.y;
+            if (Math.abs(tx) > 0.1 || Math.abs(ty) > 0.1) {
+              vx += -dir.x * ty + -right.x * tx;
+              vz += -dir.z * ty + -right.z * tx;
+            }
 
             const vel = body.getLinearVelocity();
             body.setLinearVelocity(new Ammo.btVector3(vx * speed, vel.y(), vz * speed));
